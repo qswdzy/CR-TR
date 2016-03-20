6891f0538f11bb5f6c1da8c6ad5b4dc61777e36c
qswdzy <qswdzy@163.com>
2016-03-21 02:40:26 +0800

    uppgrade

--- CR-TR/Classes/Uppgrade/Controller/QSUppgradeViewController.h
+++ CR-TR/Classes/Uppgrade/Controller/QSUppgradeViewController.h
@@ -2,12 +2,12 @@
 //  QSUppgradeViewController.h
 //  CR-TR
 //
-//  Created by qswdzy on 16/3/12.
+//  Created by qswdzy on 16/3/21.
 //  Copyright © 2016年 qswdzy. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface QSUppgradeViewController : UITabBarController
+@interface QSUppgradeViewController : UIViewController
 
 @end
